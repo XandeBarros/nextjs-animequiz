2f79f4e79dfeafccc053b45a3998a09ecc7d5874
Xande Barros <barrosalex6@gmail.com>
2021-01-27 00:11:47 -0300

    Resolvendo o problema da metatag do ReactJS e NextJS (ou tentando pelo menos, vamos ver K)

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Widget from '../src/components/Widget'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import QuizBackground from '../src/components/QuizBackground'
+import Head from 'next/head'
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -18,35 +19,55 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <QuizBackground backgroundImage = {db.bg}>
-      <QuizContainer>
-        <Widget>
-          <Widget.Header>
-            <h1>
-              Anime Quiz
-            </h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>
-              Só pergunta boa
-            </p>
-          </Widget.Content>
-        </Widget>
-        
-        <Widget>
-          <Widget.Content>
-            <h1>
-                Quizes dos miguxos
+    <>
+      <Head >
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <title>{db.title}</title>
+          <meta property="og:image" content={db.bg} key="ogimage" />
+        </Head>
+      <QuizBackground backgroundImage = {db.bg}>
+        <QuizContainer>
+          <Widget>
+            <Widget.Header>
+              <h1>
+                Anime Quiz
               </h1>
-
+            </Widget.Header>
+            <Widget.Content>
               <p>
-                Só quiz do bão
+                Só pergunta boa
               </p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/XandeBarros/nextjs-animequiz"/>
-    </QuizBackground>
+            </Widget.Content>
+          </Widget>
+          
+          <Widget>
+            <Widget.Content>
+              <h1>
+                  Quizes dos miguxos
+                </h1>
+
+                <p>
+                  Só quiz do bão
+                </p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/XandeBarros/nextjs-animequiz"/>
+      </QuizBackground>
+    </>
   );
 } 
+
+// export function Layout({pageTitle = db.title}) {
+//   return (
+//     <>
+//       <Head>
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//         <meta charSet="utf-8" />
+//         <title>Meu ovo</title>
+//       </Head>
+//     </>
+//   )
+// }
